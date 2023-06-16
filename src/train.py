0691d6a9133d16e98f6b207503d544ab8d692614
Nikola Simic <nikola7simic@gmail.com>
2023-06-16 20:11:03 +0200

    use stable_baselines3 ppo algorithm

--- src/train.py
+++ src/train.py
@@ -1,7 +1,20 @@
-from ppo import PPO
 from rlenv import ParkingEnv
+from stable_baselines3 import PPO
+
+RESOURCE_PATH = './../models/'
 
 if __name__ == "__main__":
-    env = ParkingEnv(False, False)
-    model = PPO(env)
-    model.learn(10000000)
+    env = ParkingEnv(False)
+    model = PPO("MlpPolicy", env, verbose=1)
+    
+    try:
+        model.load(RESOURCE_PATH + "parking-ppo-v1-nobs")   
+    except:
+        print("Could not load model file. New file will be created")
+
+    i = 0
+    while True:
+        print(f"Run {i}")
+        i += 1
+        model.learn(total_timesteps=25000)
+        model.save("parking-ppo-v1-nobs")    
